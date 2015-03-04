73c617abd6217b6ecb255c024d0876ad27dc73a2
matt-barton <matt@mattbarton.org>
2015-03-04 15:24:14 +0000

    Implemented player prediction report

---
+++ assets/scripts/predictions-report.js
@@ -0,0 +1,37 @@
+function initPredictionsReport() {
+
+	$('.incomplete-predictions-toggle').click(function(evt){
+		var link = $(this);
+		var index = this.getAttribute('data-index');
+		$('.incomplete-predictions-area').each(function(){
+			if (this.getAttribute('data-index') == index) {
+				$(this).toggle();
+			}
+		});
+		link.text(link.text() == '[show]' ? '[hide]' : '[show]');
+	});
+
+	$('.prediction').click(function(){
+		var predictionBox = $(this);
+		var miniseason = this.getAttribute('data-miniseason');
+		var user = this.getAttribute('data-user');
+		var details = $('.prediction-details', predictionBox);
+
+		if (predictionBox.hasClass('prediction-small')) {
+			predictionBox
+				.removeClass('prediction-small')
+				.removeClass('one-eighth')
+				.addClass('prediction-large')
+				.addClass('one-quarter');
+			details.show('slow');
+		}
+		else {
+			predictionBox
+				.removeClass('prediction-large')
+				.removeClass('one-quarter')
+				.addClass('prediction-small')
+				.addClass('one-eighth');
+			details.hide('slow');
+		}
+	});
+}
