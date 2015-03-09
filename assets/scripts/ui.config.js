cbe162e1a11148a1d2eb26ae14ba1fd349b64ddd
Matt Barton <matt@mattbarton.org>
2015-03-09 22:08:34 +0000

    Set default blockui state

--- assets/scripts/ui.config.js
+++ assets/scripts/ui.config.js
@@ -1,3 +1,11 @@
+$.blockUI.defaults.message = '<i class="fa fa-cog fa-spin fa-5x"></i>';
+$.blockUI.defaults.css['border'] = 'none'; 
+$.blockUI.defaults.css['padding'] = '15px'; 
+$.blockUI.defaults.css['backgroundColor'] = '#000';
+$.blockUI.defaults.css['webkit-border-radius'] = '10px'; 
+$.blockUI.defaults.css['-moz-border-radius'] = '10px'; 
+$.blockUI.defaults.css['opacity'] = '.5'; 
+$.blockUI.defaults.css['color'] = '#fff'; 
 
 $.datepicker.setDefaults({
 	dateFormat: 'dd M yy',
